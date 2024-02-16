8e5c23c29f92203d68963b9f53571fc9a4505dfb
teddybeargaming <160274508+teddybeargaming@users.noreply.github.com>
2024-02-16 19:17:46 +0000

    Add files via upload

---
+++ BaldisBasics143.py
@@ -0,0 +1,52 @@
+print "Baldi's Basics 1.4.3 in Python (WIP)"
+print "I am still working on it lol"
+ global mySprite, mySprite2
+    controller.player1.move_sprite(mySprite)
+    mySprite = sprites.create(img("""
+            . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . .
+        """),
+        SpriteKind.player)
+    mySprite2 = sprites.create(img("""
+            . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . .
+        """),
+        SpriteKind.projectile)
+    mySprite.set_position(49, 0)
+    if True:
+        game.game_over(True)
+        music.play(music.string_playable("- C5 G C E E G - ", 42),
+            music.PlaybackMode.UNTIL_DONE)
+controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
+
+mySprite2: Sprite = None
+mySprite: Sprite = None
+Notebooks = 0
